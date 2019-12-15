1377d1b8cb287dca7eab19661296a378656c2bda
qasims-dev <qasims.dev@gmail.com>
2019-12-15 13:44:36 -0800

    added privacy policy, statement and email.php

--- mail.php
+++ mail.php
@@ -1,1 +1,13 @@
-<?php echo "YES PHP"?>
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$to = "aqasim81@gmail.com";
+$email_from = $email;
+$email_subject = "New NECS Contact";
+$email_body = "You have received a new message from the user $name.\n"."Here is the message:\n $message".
+mail($to,$email_subject,$email_body,$headers);
+
+
+?>
